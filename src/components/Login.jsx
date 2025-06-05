9ce088cca5df7dd52f3c6aafab21fcae48cbf34e
avinay6797 <23r11a6797@gcet.edu.in>
2025-06-05 12:41:43 +0530

    updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
-import { AppContext } from "./App21";
+import { AppContext } from "../App";
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 export default function Login() {
-  const { users } = useContext(AppContext);
-  const [user, setUser] = useState({});
+  const { users, user, setUser } = useContext(AppContext);
   const [msg, setMsg] = useState();
-  const handleSubmit = () => {
-    const found = users.find(
-      (value) => value.email === user.email && value.pass === user.pass
-    );
-    if (found) {
-      setMsg("Welcome " + found.name);
+  const Navigate = useNavigate();
+  const API = import.meta.env.VITE_API_URL;
+  const handleSubmit = async () => {
+    // const found = users.find(
+    //   (value) => value.email === user.email && value.pass === user.pass
+    // );
+    const url = `${API}/users/login`;
+    const found = await axios.post(url, user);
+    if (found.data.token) {
+      setUser(found.data);
+      Navigate("/");
     } else {
       setMsg("Invalid User or Password");
     }
   };
+
+  const goToRegister = () => {
+    Navigate("/register");
+  };
+
   return (
     <div style={{ margin: "30px" }}>
       <h3>Login</h3>
@@ -34,6 +45,9 @@ export default function Login() {
         />
       </p>
       <button onClick={handleSubmit}>Submit</button>
+      <p>
+        <button onClick={goToRegister}>Create Account</button>
+      </p>
     </div>
   );
 }
